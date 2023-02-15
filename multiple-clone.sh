fcdd9becaea5291750314343921d50e33f7cf265
Yudanto Anas <youdant@gmail.com>
2023-02-15 16:12:21 +0700

    change command to use arguments for batchName and prefix

--- multiple-clone.sh
+++ multiple-clone.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 
-prefix=$(cat prefix.txt)
+batchName=$1
+prefix=$2
 
-batchName=$(cat organization.txt)
+function usage() {
+  cat <<USAGE
+    Missing some arguments
+    Usage: $0 <org_name> <prefix_name>
+
+    Example:
+        <org_name>  : h8-fsjs-gitclass-p1-23
+        <prefix>    : fsjs-p1-v1-c1
+USAGE
+  exit 1
+}
+
+# check if only 1 or none argument
+if [ $# -eq 0 ] || [ $# -eq 1 ]; then
+  usage
+  exit 1
+fi
 
 for element in $(cat usernameList.txt)
 do
